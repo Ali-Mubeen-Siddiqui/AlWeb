e12c5da1bddfda61445acae3dba4e37423f1e22b
Ali-Mubeen-Siddiqui <alimubeensiddiqui@gmail.com>
2025-01-01 14:59:16 +0530

    home page completed fully

--- webhome/models.py
+++ webhome/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Gallery(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateField()
+    image = models.ImageField(upload_to='gallery_images/')
+
+    def __str__(self):
+        return f"{self.name}   -   {self.date}  -  {self.image}"
