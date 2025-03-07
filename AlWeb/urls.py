b4b191ec8cf2ac89b99d1b0c86a87574934d285c
Ali-Mubeen-Siddiqui <alimubeensiddiqui@gmail.com>
2025-03-07 17:02:56 +0530

    last chk last

--- AlWeb/urls.py
+++ AlWeb/urls.py
@@ -18,16 +18,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.sitemaps.views import sitemap
-from webhome.sitemap import StaticSitemapView
 
-sitemaps = {
-    'static': StaticSitemapView
-}
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sitemap.xml',sitemap, {'sitemaps': sitemaps}),
+   
     path('', include('webhome.urls')),
 ]
 
